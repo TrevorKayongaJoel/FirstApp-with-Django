b4735e9072c63e09bf9875af77622f7746f1fe6b
KayongaTrevorJoel <joeltrevork@gmail.com>
2026-06-23 15:10:26 +0300

    Templates

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-
+from .forms import ReservationForm
 # Create your views here.
 
 # Function based view
@@ -12,3 +12,14 @@ def hello_world(request):
 class HelloSomalia(View):
     def get(self, request):
         return HttpResponse("Hello Somalia")
+    
+def home(request):
+    form = ReservationForm()
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Reservation created successfully!")
+        
+        return render(request, 'index.html', {'form': form})
