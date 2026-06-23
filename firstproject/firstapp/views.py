5af6605b9c31a4d51b67acc6d48ecc3e64e5be1d
KayongaTrevorJoel <joeltrevork@gmail.com>
2026-06-23 13:01:13 +0300

    rest

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
+
+# Function based view
+def hello_world(request):
+    return HttpResponse("Hello World")
+
+# class based view, we use inheritance to create a class based view
+class HelloSomalia(View):
+    def get(self, request):
+        return HttpResponse("Hello Somalia")
